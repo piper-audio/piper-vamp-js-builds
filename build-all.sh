0da7525ded1800c524d9604b992fd99a825ad371
Chris Cannam <c.cannam@qmul.ac.uk>
2016-11-10 17:43:03 +0000

    Run simple tests

--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+clean=""
+if [ "$1" = "clean" ]; then
+    clean=yes
+fi
+
 set -eu
 
 for x in *; do
-    if [ -d "$x" ]; then
-        if [ -f "$x/Makefile" ]; then
-            make -C "$x" distclean
-            make -C "$x" em
-        fi
+    if [ -d "$x" ] && [ -f "$x/Makefile" ]; then
+	if [ -n "$clean" ]; then
+	    make -C "$x" clean
+	fi
+        make -C "$x" em
     fi
 done
 
 echo "Compiled:"
 ls -l */*.js
+
+for x in *; do
+    if [ -d "$x" ] && [ -f "$x/Makefile" ]; then
+	echo
+	js=$(echo "$x"/*.js)
+	if [ ! -f "$js" ]; then
+	    echo "Problem running test for $x: more than one js file? (js=$js)"
+	else
+	    node ../piper-vamp-js/test/node-load-test.js "$(pwd)/$js" && \
+		echo "Successfully ran basic load-test on $js"
+	fi
+    fi
+done
+
+echo
+echo "All done"
+
